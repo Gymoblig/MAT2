7d37e89b4ba2ae062cdc5f6ff8d170175d222187
GreenyBeany <74238246+Gymoblig@users.noreply.github.com>
2024-05-14 00:01:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ function downloadPDF(filename) {
     // Add event listener to close overlay when clicked outside the PDF
     overlay.addEventListener('click', function(event) {
         if (event.target === overlay) {
-            // Remove overlay and iframe
-            document.body.removeChild(overlay);
+            // Add smooth removal class
+            overlay.classList.add('remove-overlay');
+            // Remove overlay and iframe after a brief delay
+            setTimeout(() => {
+                document.body.removeChild(overlay);
+            }, 300); // Adjust the time according to your transition duration
         }
-    });  
-  }
-  
+    });
+}
